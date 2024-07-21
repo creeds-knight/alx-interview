5fa9026d7ba25bfdde9a70989bbd21a3b5939ba5
creeds-knight <apedoarthur21@gmail.com>
2024-07-21 17:53:24 +0300

    adding one line logic

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,21 +3,12 @@
     A Script to parse logs from stdin and return formatted output
 """
 import sys
-import re
 import signal
 
 total_file_size = 0
 list_of_codes = [200, 301, 499, 401, 403, 404, 405, 500]
 status_codes = {code: 0 for code in list_of_codes}
 line_count = 0
-
-pattern = re.compile(
-    r'\s*(?P<ip>\S+)\s*'
-    r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]'
-    r'\s*"(?P<request>[^"]*)"\s*'
-    r'\s*(?P<status_code>\d+)\s*'
-    r'\s*(?P<file_size>\d+)'
-)
 
 
 def print_stats():
@@ -40,27 +31,42 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 
-try:
-    for line in sys.stdin:
-        parts = line.split()
-        protocol = parts[6][:-1]
-        status_code = parts[7]
-        file_size = parts[8]
-        # print(protocol, status_code, file_size)
+def run():
+    """ Main entry point to program """
 
-        try:
-            status_code = int(status_code)
-            file_size = int(file_size)
-        except ValueError:
-            continue
+    global total_file_size
+    global status_codes
+    global line_count
 
-        if status_code in status_codes:
-            status_codes[status_code] += 1
-        total_file_size += file_size
+    try:
+        for line in sys.stdin:
+            parts = line.split()
+            if len(parts) < 9:
+                continue
 
-        line_count += 1
+            status_code = parts[7]
+            file_size = parts[8]
 
-        if line_count % 10 == 0:
-            print_stats()
-except Exception as e:
-    print(str(e))
+            try:
+                status_code = int(status_code)
+                file_size = int(file_size)
+            except ValueError:
+                continue
+
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+
+            total_file_size += file_size
+
+            line_count += 1
+
+            if line_count % 10 == 0:
+                print_stats()
+        print_stats()
+
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    run()
