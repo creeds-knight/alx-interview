d9119e5028633827bd856f629c94f01ddf7eb909
creeds-knight <apedoarthur21@gmail.com>
2024-07-31 09:45:46 +0300

    Nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -48,11 +48,13 @@ if __name__ == "__main__":
         print("Usage: nqueens N")
         sys.exit(1)
 
-    N = int(sys.argv[1])
-    if not isinstance(N, int):
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
         print("N must be a number")
         sys.exit(1)
-    elif N < 4:
+
+    if N < 4:
         print("N must be at least 4")
         sys.exit(1)
 
